bd4282f4fff0addc378825e388caf0b3cb255b86
NiJo89 <j.nissen@right-basedonscience.de>
2025-03-14 16:48:21 +0100

    :sparkles: update layout and main page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,30 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="column items-center justify-center">
+    <q-banner>
+      <p class="text-h6 text-center">
+        Nice to meet you. <br />
+        Please select one of the inputs below.
+      </p>
+    </q-banner>
+    <q-card>
+      <q-tabs
+        v-model="selectedTab"
+        class="bg-grey-2 text-grey-7"
+        active-color="primary"
+        indicator-color="purple"
+        align="justify"
+      >
+        <q-tab name="variant1" label="Variant 1" />
+        <q-tab name="variant2" label="Variant 2" />
+        <q-tab name="variant3" label="Variant 3" />
+      </q-tabs>
+ADD INPUTS HERE
+    </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+const selectedTab = ref('variant1');
 </script>
